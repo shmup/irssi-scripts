1060b63c224dae4f0d1bc43a2809573f8b1eef19
Jared Miller <jtmiller@gmail.com>
2013-07-24 21:57:14 -0400

    updated crawl to proper codebase

--- buttcoin.pl
+++ buttcoin.pl
@@ -13,14 +13,27 @@ $VERSION = '1.00';
     authors     => 'Jared Tyler Miller',
     contact     => 'jtmiller@gmail.com',
     name        => 'buttcoin',
+<<<<<<< .merge_file_eS4f0i
     description => 'prints out btc value/difference every x interval',
+=======
+    description => 'spits out new markers for buttcoin value',
+>>>>>>> .merge_file_pEkC0n
     license     => 'Public Domain',
 );
 
 print CLIENTCRAP "loading buttcoin.pl $VERSION!";
 
+<<<<<<< .merge_file_eS4f0i
 Irssi::settings_add_str("buttcoin", "buttcoin_interval", "");
 my $interval = Irssi::settings_get_str("buttcoin_interval") || 300000;
+=======
+sub message {
+    my $msg = shift;
+    my $server_name = 'freenode';
+    my $server = Irssi::server_find_tag($server_name);
+    $server->command("MSG $channel_name $msg");
+}
+>>>>>>> .merge_file_pEkC0n
 
 sub check_buttcoins {
     # grab the data
@@ -31,6 +44,7 @@ sub check_buttcoins {
     my $old_score = get_score() || 0;
     # grab the current highest value
     my $new_score = $data->{return}{last}{value};
+<<<<<<< .merge_file_eS4f0i
     # save the new score
     set_score($new_score);
     # grab the difference
@@ -45,10 +59,37 @@ sub check_buttcoins {
 
 sub get_score {
     return Irssi::settings_get_str("value");
+=======
+
+    return unless $new_score != $old_score;
+
+    # save the new score
+    set_score($new_score);
+    # grab the difference
+    my $difference = $new_score - $old_score;
+    # grab prettier score
+    my $pretty_score = $data->{return}{last}{display};
+
+    my $msg;
+
+    if ($difference > 0) {
+        $msg = $pretty_score . ' (+'.sprintf("%.5f", $difference).')';
+    } elsif ($difference < 0)  {
+        $msg = $pretty_score . ' ('.sprintf("%.5f", $difference).')';
+    }
+
+    my $win = Irssi::active_win();
+    $win->print("BTC $msg", 0);
+}
+
+sub get_score {
+    return Irssi::settings_get_str("highscore");
+>>>>>>> .merge_file_pEkC0n
 }
 
 sub set_score {
     my $score = shift;
+<<<<<<< .merge_file_eS4f0i
     Irssi::settings_set_str("value", $score);
 }
 
@@ -59,3 +100,21 @@ check_buttcoins(); # run first time you load script
 
 print CLIENTCRAP "/set buttcoin_interval 250000 (in milliseconds)";
 print CLIENTCRAP "buttcoin check interval: " . $interval . "ms";
+=======
+    Irssi::settings_set_str("highscore", $score);
+}
+
+
+
+# Irssi::signal_add("message public", "dispatch");
+# Irssi::signal_add("message private", "priv_dispatch");
+Irssi::settings_add_str("buttcoin", "highscore", "");
+
+check_buttcoins();
+
+# Irssi::settings_add_str("crawlwatch", "bannersearned", "");
+# Irssi::timeout_add(60000, 'check_banners', ''); # check for new banners every 5 minutes
+
+# print CLIENTCRAP "/set crawlwatchnicks ed edd eddy ...";
+# print CLIENTCRAP "Watched nicks: " . Irssi::settings_get_str("crawlwatchnicks");
+>>>>>>> .merge_file_pEkC0n
