f4af7a0f01a7a3a344e3df974456442be6ace392
Jared Miller <jared@smell.flowers>
2023-02-23 11:26:45 -0500

    Refactor into cleaner code

--- raku-discord-bot-renamer.pl
+++ raku-discord-bot-renamer.pl
@@ -1,5 +1,5 @@
 # rakudiscord.pl
-# Rename the <raku-discord-bot> to the <discord_nick>
+# Replace `<raku-discord-bot> <nick>` with `<nick>`
 
 use strict;
 use vars qw($VERSION %IRSSI);
@@ -10,7 +10,7 @@ $VERSION = '1.01';
   authors     => 'Jared Miller <shmup>',
   contact     => 'jared@smell.flowers',
   name        => 'raku-discord-bot-renamer',
-  description => 'Replace <discord-raku-bot> with <$discord_nick>',
+  description => 'Replace `<discord-raku-bot> <nick>` with `<nick>`',
   license     => 'Artistic-2.0',
   url         => 'https://github.com/shmup/irssi-scripts',
   changed     => '2023-02-22',
@@ -20,20 +20,16 @@ my @channels = ("#raku", "#raku-dev", "#raku-beginner", "#moarvm", "#webring");
 
 sub rename_botnick {
   my ($server, $data, $nick, $address, $target) = @_;
-  if ($nick ne "discord-raku-bot") { return }; # get out
+  if ($nick ne "discord-raku-bot") { return }; # not a bot
 
   my ($channel, $msg) = split(/ :/, $data, 2);
-  my $oldnick = $nick;
+  if (!grep($channel, @channels)) { return }; # wrong channel
 
-  for (@channels) {
-    if ($_ eq $channel) {
-      my ($discord, $txt) = $msg =~ /^<(.*)>\s(.*)/;
-      if ($oldnick eq $discord) { continue }; # fail safe
+  my ($discord, $txt) = $msg =~ /^<(.*)>\s(.*)/;
+  if ($discord eq "") { return }; # something goofy. pattern didn't match
 
-      Irssi::signal_emit("event privmsg", $server, "$channel :$txt", $discord, $address, $target);
-      Irssi::signal_stop();
-    }
-  }
+  Irssi::signal_emit("event privmsg", $server, "$channel :$txt", $discord, $address, $target);
+  Irssi::signal_stop();
 }
 
 Irssi::signal_add('event privmsg', 'rename_botnick');
