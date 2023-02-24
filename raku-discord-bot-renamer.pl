55aa1eccaf55a8382168ce4f6c177e52ad4847f5
Jared Miller <jared@smell.flowers>
2023-02-24 14:19:57 -0500

    Add a comment explaining how to load an irssi script

--- raku-discord-bot-renamer.pl
+++ raku-discord-bot-renamer.pl
@@ -1,6 +1,11 @@
 # rakudiscord.pl
 # Replace `<raku-discord-bot> <nick>` with `<nick>`
 
+# Place this in your $HOME/.irssi/scripts/
+# You can load with: /script load raku-discord-bot-renamer
+# If you place this in /scripts/autorun/ then it'll start on launch.
+# https://irssi.org/documentation/help/script/
+
 use strict;
 use vars qw($VERSION %IRSSI);
 use Irssi;
@@ -13,7 +18,7 @@ $VERSION = '1.01';
   description => 'Replace `<discord-raku-bot> <nick>` with `<nick>`',
   license     => 'Artistic-2.0',
   url         => 'https://github.com/shmup/irssi-scripts',
-  changed     => '2023-02-22',
+  changed     => '2023-02-24',
 );
 
 my @channels = ("#raku", "#raku-dev", "#raku-beginner", "#moarvm", "#webring");
@@ -22,13 +27,13 @@ sub rename_botnick {
   my ($server, $data, $nick, $address, $target) = @_;
   if ($nick ne "discord-raku-bot") { return }; # not a bot
 
-  my ($channel, $msg) = split(/ :/, $data, 2);
+  my ($channel, $msg) = split(/ :/, $data, 2
   if (!grep($channel, @channels)) { return }; # wrong channel
 
   my ($discord, $txt) = $msg =~ /^<(.*)>\s(.*)/;
   if ($discord eq "") { return }; # something goofy. pattern didn't match
 
-  Irssi::signal_emit("event privmsg", $server, "$channel :$txt", $discord, $address, $target);
+  Irssi::signal_emit("event privmsg", $server, "$channel :$txt", $discord, $address, $targe
   Irssi::signal_stop();
 }
 
