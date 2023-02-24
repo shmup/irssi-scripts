b15aaeedb14481ff3d358864b71ee767b8e3cce9
Jared Miller <jared@smell.flowers>
2023-02-24 17:14:10 -0500

    Print the script version on load and clean up formatting

--- discord-raku-bot-renamer.pl
+++ discord-raku-bot-renamer.pl
@@ -1,8 +1,8 @@
-# rakudiscord.pl
-# Replace `<raku-discord-bot> <nick>` with `<nick>`
+# discord-raku-bot-renamer.pl
+# Replace `<discord-raku-bot> <nick>` with `<nick>`
 
 # Place this in your $HOME/.irssi/scripts/
-# You can load with: /script load raku-discord-bot-renamer
+# You can load with: /script load discord-raku-bot-renamer
 # If you place this in /scripts/autorun/ then it'll start on launch.
 # https://irssi.org/documentation/help/script/
 
@@ -10,11 +10,11 @@ use strict;
 use vars qw($VERSION %IRSSI);
 use Irssi;
 
-$VERSION = '1.02';
-%IRSSI = (
+$VERSION = '1.03';
+%IRSSI   = (
   authors     => 'Jared Miller <shmup>',
   contact     => 'jared@smell.flowers',
-  name        => 'raku-discord-bot-renamer',
+  name        => 'discord-raku-bot-renamer',
   description => 'Replace `<discord-raku-bot> <nick>` with `<nick>`',
   license     => 'Artistic-2.0',
   url         => 'https://github.com/shmup/irssi-scripts',
@@ -25,16 +25,21 @@ my @channels = ("#raku", "#raku-dev", "#raku-beginner", "#moarvm", "#webring");
 
 sub rename_botnick {
   my ($server, $data, $nick, $address, $target) = @_;
-  if ($nick ne "discord-raku-bot") { return }; # not a bot
+  if ($nick ne "discord-raku-bot") {return}; # not a bot
 
   my ($channel, $msg) = split(/ :/, $data, 2);
-  if (!grep($channel, @channels)) { return }; # wrong channel
+  if (!grep($channel, @channels)) {return}; # wrong channel
 
   my ($discord, $txt) = $msg =~ /^<(.*)>\s(.*)/;
-  if ($discord eq "") { return }; # something goofy. pattern didn't match
+  if ($discord eq "" or $txt eq "") {return}; # no reason to do anything
 
-  Irssi::signal_emit("event privmsg", $server, "$channel :$txt", $discord, $address, $target);
+  Irssi::signal_emit(
+    "event privmsg",
+    $server,  "$channel :$txt",
+    $discord, $address, $target
+  );
   Irssi::signal_stop();
 }
 
+print CLIENTCRAP "Loaded $IRSSI{'name'} v$VERSION";
 Irssi::signal_add('event privmsg', 'rename_botnick');
