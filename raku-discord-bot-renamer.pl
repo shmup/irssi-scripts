9e5aa2716a6e1982c1b85bf48f840800994b22df
Jared Miller <jared@smell.flowers>
2023-02-24 15:15:50 -0500

    Bump the version and fix a typo-bug

--- raku-discord-bot-renamer.pl
+++ raku-discord-bot-renamer.pl
@@ -10,7 +10,7 @@ use strict;
 use vars qw($VERSION %IRSSI);
 use Irssi;
 
-$VERSION = '1.01';
+$VERSION = '1.02';
 %IRSSI = (
   authors     => 'Jared Miller <shmup>',
   contact     => 'jared@smell.flowers',
@@ -27,13 +27,13 @@ sub rename_botnick {
   my ($server, $data, $nick, $address, $target) = @_;
   if ($nick ne "discord-raku-bot") { return }; # not a bot
 
-  my ($channel, $msg) = split(/ :/, $data, 2
+  my ($channel, $msg) = split(/ :/, $data, 2);
   if (!grep($channel, @channels)) { return }; # wrong channel
 
   my ($discord, $txt) = $msg =~ /^<(.*)>\s(.*)/;
   if ($discord eq "") { return }; # something goofy. pattern didn't match
 
-  Irssi::signal_emit("event privmsg", $server, "$channel :$txt", $discord, $address, $targe
+  Irssi::signal_emit("event privmsg", $server, "$channel :$txt", $discord, $address, $target);
   Irssi::signal_stop();
 }
 
